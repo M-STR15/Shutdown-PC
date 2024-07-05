f14602b44b4096c0d886b69519653a781431b3cb
M-Str <miroslav.stransky@live.com>
2024-07-05 11:52:52 +0200

    CC

---
+++ Shutdown-PC/Controls/ButtonsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shutdown_PC.Controls
+{
+    /// <summary>
+    /// Interaction logic for ButtongControl.xaml
+    /// </summary>
+    public partial class ButtonsControl : UserControl
+    {
+        public ButtonsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
