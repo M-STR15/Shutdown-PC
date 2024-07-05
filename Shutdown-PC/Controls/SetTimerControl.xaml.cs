6effa475896e0735399615953fa54cb5ca8bc0db
M-Str <miroslav.stransky@live.com>
2024-07-05 13:16:23 +0200

    MOD: přidán nový ovládací prvek

---
+++ Shutdown-PC/Controls/SetTimerControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shutdown_PC.Controls
+{
+    /// <summary>
+    /// Interaction logic for SetTimerControl.xaml
+    /// </summary>
+    public partial class SetTimerControl : UserControl
+    {
+        public SetTimerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
