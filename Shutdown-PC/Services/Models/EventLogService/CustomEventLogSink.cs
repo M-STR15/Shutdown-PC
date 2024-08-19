dd5a4475e769ade85d8b8fcfec078efb1b8a1b60
M-Str <miroslav.stransky@live.com>
2024-08-19 18:37:46 +0200

    MOD

--- Shutdown-PC/Services/Models/EventLogService/CustomEventLogSink.cs
+++ Shutdown-PC/Services/Models/EventLogService/CustomEventLogSink.cs
@@ -10,7 +10,6 @@ namespace ShutdownPC.Services.Models.EventLogService
 	public class CustomEventLogSink : ILogEventSink
 	{
 		private readonly string _source;
-		private readonly EventLog _eventLog;
 
 		public CustomEventLogSink(string source)
 		{
@@ -19,26 +18,37 @@ namespace ShutdownPC.Services.Models.EventLogService
 			{
 				EventLog.CreateEventSource(source, "Application");
 			}
-			_eventLog = new EventLog("Application") { Source = source };
 		}
 
 		public void Emit(LogEvent logEvent)
 		{
-			var sb = new StringBuilder();
-			sb.AppendLine("<Event xmlns=\"http://schemas.microsoft.com/win/2004/08/events/event\">");
-			sb.AppendLine("<System>");
-			sb.AppendLine($"<Provider Name=\"{_source}\" />");
-			sb.AppendLine($"<EventID>{logEvent.GetHashCode()}</EventID>");
-			sb.AppendLine($"<Version>{logEvent.Properties.GetValueOrDefault("SoftwareVersion")?.ToString() ?? "Unknown"}</Version>"); // Přidání verze jako statické hodnoty
-			sb.AppendLine("</System>");
-			sb.AppendLine("<EventData>");
-			sb.AppendLine($"<Data Name=\"Message\">{logEvent.RenderMessage()}</Data>");
-			sb.AppendLine($"<Data Name=\"SoftwareVersion\">{logEvent.Properties.GetValueOrDefault("SoftwareVersion")?.ToString() ?? "Unknown"}</Data>");
-			sb.AppendLine("</EventData>");
-			sb.AppendLine("</Event>");
+			var message = logEvent.RenderMessage();
 
-			var result= sb.ToString();
-			_eventLog.WriteEntry(sb.ToString(), GetEventLogEntryType(logEvent.Level));
+			// Map Serilog log level to EventLogEntryType
+			EventLogEntryType entryType;
+			switch (logEvent.Level)
+			{
+				case LogEventLevel.Information:
+					entryType = EventLogEntryType.Information;
+					break;
+				case LogEventLevel.Warning:
+					entryType = EventLogEntryType.Warning;
+					break;
+				case LogEventLevel.Error:
+				case LogEventLevel.Fatal:
+					entryType = EventLogEntryType.Error;
+					break;
+				default:
+					entryType = EventLogEntryType.Information;
+					break;
+			}
+
+			// Write to Event Log
+			using (EventLog eventLog = new EventLog("Application"))
+			{
+				eventLog.Source = _source;
+				eventLog.WriteEntry(message, entryType);
+			}
 		}
 
 		private EventLogEntryType GetEventLogEntryType(LogEventLevel level)
