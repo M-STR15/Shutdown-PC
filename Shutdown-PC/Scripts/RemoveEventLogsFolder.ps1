b1379d36dba799b59b5bf6f0b5ae961b7d2d6c34
M-Str <miroslav.stransky@live.com>
2024-08-20 20:38:39 +0200

    MOD: přidán script pro mazání logů

---
+++ Shutdown-PC/Scripts/RemoveEventLogsFolder.ps1
@@ -0,0 +1,11 @@
+﻿# Cesta ke složce, kterou chcete smazat
+$slozka = Join-Path -Path (Resolve-Path $PSScriptRoot\..) -ChildPath "bin\Debug\net8.0-windows\logs"
+
+# Zkontroluje, zda složka existuje
+if (Test-Path $slozka) {
+    # Smaže složku a její obsah
+    Remove-Item -Path $slozka -Recurse -Force
+    Write-Host "Složka byla úspěšně smazána."
+} else {
+    Write-Host "Složka neexistuje."
+}
