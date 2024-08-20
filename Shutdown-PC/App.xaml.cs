0951b3705e9a9e124623451e45a143fc614e869b
M-Str <miroslav.stransky@live.com>
2024-08-20 20:17:42 +0200

    MOD: přidaní try/catch pro zachytávání neošetřených chyb v aplikaci, tak se to zachytí na úrovni APP

--- Shutdown-PC/App.xaml.cs
+++ Shutdown-PC/App.xaml.cs
@@ -13,12 +13,18 @@ namespace ShutdownPC
 	public partial class App : Application, IDisposable
 	{
 		private IKernel _container;
-
+		private EventLogService _log;
 		public App()
 		{ }
 
 		protected override void OnStartup(StartupEventArgs e)
 		{
+			_log = new EventLogService();
+			// Zachycení neošetřených výjimek na úrovni aplikačního vlákna
+			AppDomain.CurrentDomain.UnhandledException += currentDomain_UnhandledException;
+			// Zachycení neošetřených výjimek na úrovni dispatcheru (UI vlákno)
+			DispatcherUnhandledException += app_DispatcherUnhandledException;
+
 			configureContainer();
 
 			InitializeComponent();
@@ -74,6 +80,23 @@ namespace ShutdownPC
 			var logger = _container.Get<EventLogService>();
 			if (logger != null)
 				logger.Dispose();
+		}
+
+		private void app_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			_log.Error(Guid.Parse("c9c951ff-f176-4bcc-be5e-a87a9920c3e1"), $"Neočekávaná chyba (UI vlákno): {e.Exception.Message}");
+
+			// Nastavení e.Handled na true zabrání aplikaci spadnout
+			e.Handled = false;
+		}
+
+		private void currentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			if (e.ExceptionObject is Exception ex)
+				_log.Error(Guid.Parse("4fb577ad-9eba-4afa-9a89-368268989360"), $"Neočekávaná chyba (jiné vlákno): {ex.Message}");
+			else
+				_log.Error(Guid.Parse("fbc0e288-2f92-45e7-a8fc-2c5136c0dec0"), $"\"Došlo k neošetřené výjimce, která není typu Exception.");
+
 		}
 	}
 }
